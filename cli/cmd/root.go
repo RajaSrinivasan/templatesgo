e2c9ef00c94259a293cea699ee336e9beb843faa
Srini <s@srin.me>
2020-04-02 16:42:24 -0400

    missing files

---
+++ cli/cmd/root.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+var verbose bool
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "template",
+	Short: "Command Line utilities template",
+	Long: `
+	This is a template project for command line utilities
+	`,
+	Version: "v0.0.0",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli/cli.yaml)")
+	rootCmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "be verbose")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defconfigpath := path.Join(home, ".cli")
+		viper.AddConfigPath(defconfigpath)
+		viper.AddConfigPath("./etc")
+		viper.SetConfigName("cli")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
