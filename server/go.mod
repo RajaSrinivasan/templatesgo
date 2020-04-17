07f1434cdd549701587f011dfe988a8f6f552ba3
Srini <s@srin.me>
2020-04-17 18:12:02 -0400

    provide some basic stats

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,7 @@ module gitlab.com/projtemplates/go/server
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.6.2
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.6.3
--- server/https/serve/serve.go
+++ server/https/serve/serve.go
@@ -3,9 +3,19 @@ package serve
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var startTime string
+
+func getStats(c *gin.Context) {
+	timenow := time.Now().Format(time.ANSIC)
+	hostname, _ := os.Hostname()
+	c.String(http.StatusOK, "Hostname : %s\nStarted %s\nTime Now %s\n", hostname, startTime, timenow)
+}
 
 func getTop(c *gin.Context) {
 	c.HTML(
@@ -20,10 +30,11 @@ func getTop(c *gin.Context) {
 
 func ProvideService(certfn, pvtkeyfn, hostnport string, htmlpath string) {
 	log.Printf("Providing Service HTTPS")
-
+	startTime = time.Now().Format(time.ANSIC)
 	r := gin.Default()
 	r.LoadHTMLGlob(htmlpath + "/*")
 	r.GET("/", getTop)
+	r.GET("/stats", getStats)
 	privatekeyfile := pvtkeyfn + ".pvt.pem"
 	r.RunTLS(hostnport, certfn, privatekeyfile)
 
