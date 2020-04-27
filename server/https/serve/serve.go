912f7884e5820b3b883a65620a1a5f5394f8798a
Srini <s@srin.me>
2020-04-27 17:06:53 -0400

    eliminate gin related functions

--- server/https/serve/serve.go
+++ server/https/serve/serve.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gorilla/mux"
 	"gitlab.com/projtemplates/go/server/version"
 )
@@ -33,52 +32,6 @@ type Stats struct {
 
 	BuildTime string
 	Version   string
-}
-
-func getStats(c *gin.Context) {
-	timenow := time.Now().Format(time.ANSIC)
-	hostname, _ := os.Hostname()
-	//c.String(http.StatusOK, "Hostname : %s\nStarted %s\nTime Now %s\n", hostname, startTime, timenow)
-
-	c.HTML(
-		http.StatusOK,
-		"stats.html",
-		gin.H{
-			"title":        "Server Stats",
-			"hostname":     hostname,
-			"time_start":   startTime,
-			"time_now":     timenow,
-			"build_time":   version.BuildTime,
-			"major":        version.VersionMajor,
-			"minor":        version.VersionMinor,
-			"build":        version.VersionBuild,
-			"repo":         version.RepoURL,
-			"branch":       version.BranchName,
-			"short_commit": version.ShortCommitId,
-			"long_commit":  version.LongCommitId},
-	)
-}
-
-func getTop(c *gin.Context) {
-	c.HTML(
-		http.StatusOK,
-		"index.html",
-		gin.H{
-			"title": "TOPR LLC.",
-		},
-	)
-
-}
-
-func ProvideGinService(certfn, pvtkeyfn, hostnport string, htmlpath string) {
-	log.Printf("Providing Service using gin HTTPS")
-	startTime = time.Now().Format(time.ANSIC)
-	r := gin.Default()
-	r.LoadHTMLGlob(htmlpath + "/*")
-	r.GET("/", getTop)
-	r.GET("/stats", getStats)
-	privatekeyfile := pvtkeyfn + ".pvt.pem"
-	r.RunTLS(hostnport, certfn, privatekeyfile)
 }
 
 func getGorillaStats(w http.ResponseWriter, r *http.Request) {
