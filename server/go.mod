6781987b8be91b98377dd0faf99a517114125381
Srini <s@srin.me>
2020-04-27 17:04:58 -0400

    migration to gorilla mux

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.6.2
+	github.com/gorilla/mux v1.7.4
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
--- server/https/serve/serve.go
+++ server/https/serve/serve.go
@@ -1,6 +1,8 @@
 package serve
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +14,26 @@ import (
 )
 
 var startTime string
+var templates *template.Template
+
+type Info struct {
+	Title string
+}
+
+type Stats struct {
+	Title       string
+	Hostname    string
+	TimeStart   string
+	TimeNow     string
+	BuilTTime   string
+	Repo        string
+	Branch      string
+	ShortCommit string
+	LongCommit  string
+
+	BuildTime string
+	Version   string
+}
 
 func getStats(c *gin.Context) {
 	timenow := time.Now().Format(time.ANSIC)
@@ -60,16 +82,56 @@ func ProvideGinService(certfn, pvtkeyfn, hostnport string, htmlpath string) {
 }
 
 func getGorillaStats(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Stats"))
+	stats := templates.Lookup("stats.html")
+	if stats == nil {
+		log.Printf("Unable to locate stats.html")
+		w.Write([]byte("Stats"))
+		return
+	}
+	timenow := time.Now().Format(time.ANSIC)
+	hostname, _ := os.Hostname()
+	var st = Stats{
+		TimeStart:   startTime,
+		TimeNow:     timenow,
+		Hostname:    hostname,
+		BuilTTime:   version.BuildTime,
+		Branch:      version.BranchName,
+		Version:     fmt.Sprintf("%d.%d.%d", version.VersionMajor, version.VersionMinor, version.VersionBuild),
+		Repo:        version.RepoURL,
+		ShortCommit: version.ShortCommitId,
+		LongCommit:  version.LongCommitId,
+	}
+
+	err := stats.Execute(w, st)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Served stats")
 }
 
 func getGorillaTop(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("index"))
+	index := templates.Lookup("index.html")
+	if index == nil {
+		log.Printf("Cannot find index.html")
+		w.Write([]byte("index"))
+		return
+	}
+	var info = Info{Title: "TOPR LLC"}
+	err := index.Execute(w, info)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Served index")
 }
 
 func ProvideService(certfn, pvtkeyfn, hostnport string, htmlpath string) {
 	log.Printf("Providing Service using gorilla mux HTTPS")
 	startTime = time.Now().Format(time.ANSIC)
+	var err error
+	templates, err = template.ParseGlob(htmlpath + "/*")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", getGorillaTop)
@@ -77,7 +139,7 @@ func ProvideService(certfn, pvtkeyfn, hostnport string, htmlpath string) {
 
 	http.Handle("/", r)
 
-	err := http.ListenAndServeTLS(
+	err = http.ListenAndServeTLS(
 		hostnport,
 		certfn,
 		pvtkeyfn+".pvt.pem",
